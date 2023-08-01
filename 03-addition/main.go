36b20a01b1e94a66ce0e77cd1786683a9166ea8d
Unni P <unnipofficial@outlook.com>
2023-08-01 09:08:51 +0530

    added program for addition of two numbers

---
+++ 03-addition/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// declaring variables
+	var a int
+	var b int
+	var c int
+
+	// getting input from user
+	fmt.Println("Enter the first number")
+	fmt.Scan(&a)
+	fmt.Println("Enter the second number")
+	fmt.Scan(&b)
+
+	// adding the numbers
+	c = a + b
+
+	// displaying the result
+	fmt.Printf("Result = %d", c)
+}
+
+/*
+$ go run main.go
+Enter the first number
+1
+Enter the second number
+2
+Result = 3
+*/
